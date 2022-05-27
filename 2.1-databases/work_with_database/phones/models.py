099927dfa3725c17d78cb7b08dcb56af25330706
Anastasia Lunina <lunina.anastasiya@gmail.com>
2022-05-27 17:33:13 -0400

    Modify 2.1

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    image = models.URLField()
+    release_date = models.DateTimeField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
+
+
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +8,23 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort = request.GET.get('sort')
+    sort_dict = {'name': 'name', 'min_price': 'price', 'max_price': '-price'}
+    if sort:
+        item = Phone.objects.all().order_by(sort_dict.get(sort))
+    else:
+        item = Phone.objects.all()
+
+    context = {
+        'phones': item
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    item = Phone.objects.filter(slug=slug)
+    context = {
+        'phones': item
+    }
     return render(request, template, context)
